a2efed8155de3d278bf9a6cdbca9b2cf340c4e19
Cai Jenkins <Caisjenkins@gmail.com>
2019-07-07 13:24:11 +0100

    'added a setsail function

--- Cruise-Ships/__Test__/ship.test.js
+++ Cruise-Ships/__Test__/ship.test.js
@@ -5,4 +5,16 @@ describe('ship', () => {
     it('can be instantiated', () => {
         expect(new Ship()).toBeInstanceOf(Object);
     });
+    it('Has a starting point', () => {
+        const ship = new Ship('Titanic');
+
+        expect(ship.startingPort).toBe('Hull')
+    });
+    it('can set sail', () => {
+        const ship = new Ship('Titanic');
+
+        ship.setSail();
+
+        expect(ship.startingPort).toBeFalsy();
+    });
 });
--- Cruise-Ships/src/Ship.js
+++ Cruise-Ships/src/Ship.js
@@ -1,6 +1,14 @@
 const ship = new Ship('Titanic');
 
+
 function Ship(name){
+    this.startingPort = 'Hull';
+};
+
+Ship.prototype ={
+    setSail: function() {
+        this.startingPort = undefined;
+    },
 
 };
 
